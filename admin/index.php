9827b8e79a13338a4a57bb2771ac025c5a871380
Abdulaziz Yesuf <abdulazizyesuf7@gmail.com>
2019-12-23 08:53:05 +0300

    frontend for admin login

---
+++ admin/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../assets/bootstrap-4.3.1-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <script src="../assets/js/jquery.min.js"></script>
+    <title>Admin</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+
+            </div>
+            <div class="col-sm-4 admin-login card">
+                <center>
+                    <div class="card-header">
+                        <h3>Wabi cars Admin</h3>
+                    </div>
+                </center>
+                <div class="card-body">
+                    <strong><h5>Login</h5><strong>
+                    <form class="form-group">
+                        <label for="username">username:</label>
+                        <input type="text" placeholder="username" name="username" class="form-control">
+                        <label for="password">password:</label>
+                        <input type="password" placeholder="password" name="password" class="form-control"><br>
+                        <input type="submit" class="btn btn-primary" value="login">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
