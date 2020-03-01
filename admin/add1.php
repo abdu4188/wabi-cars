c26bb38e97dfc88f46b6b390b5e77797bfeb0e82
Abdulaziz Yesuf <abdulazizyesuf7@gmail.com>
2020-03-01 11:26:33 +0300

    change image names to current datetime

--- admin/add1.php
+++ admin/add1.php
@@ -2,72 +2,58 @@
     require 'session.php';
     require '../assets/php/db.php';
 
+    $image1 = '';
     $image2 = '';
     $image3 = '';
     $image4 = '';
     $image5 = '';
     $image6 = '';
 
-    echo "<script>console.log('uploaded')</script>";
-
-
     $target_dir = "../uploads/";
-    $stmt = "SELECT * FROM image_last_name ORDER BY name DESC LIMIT 1";
-    if($result = mysqli_query($conn, $stmt)){
-        $row = mysqli_fetch_array($result);
-        $last_name = $row['name'];
-    }
-
     if (!empty($_FILES['image1']['name'])) {
         $temp = explode(".", $_FILES["image1"]["name"]);
-        $newName = $last_name + 1;
+        $newName = date('Ymdhis')."1";
         $newName = strval($newName) . "." . end($temp);
-        $last_name += 1;
         move_uploaded_file($_FILES['image1']['tmp_name'], $target_dir . $newName);
         $image1 = $newName;
     }
 
     if (!empty($_FILES['image2']['name'])) {
         $temp = explode(".", $_FILES["image2"]["name"]);
-        $newName = $last_name + 1;
+        $newName = date('Ymdhis')."2";
         $newName = strval($newName) . "." . end($temp);
-        $last_name += 1;
         move_uploaded_file($_FILES['image2']['tmp_name'], $target_dir . $newName);
         $image2 = $newName;
     }
 
     if (!empty($_FILES['image3']['name'])) {
         $temp = explode(".", $_FILES["image3"]["name"]);
-        $newName = $last_name + 1;
+        $newName = date('Ymdhis')."3";
         $newName = strval($newName) . "." . end($temp);
-        $last_name += 1;
         move_uploaded_file($_FILES['image3']['tmp_name'], $target_dir . $newName);
         $image3 = $newName;
     }
 
     if (!empty($_FILES['image4']['name'])) {
         $temp = explode(".", $_FILES["image4"]["name"]);
-        $newName = $last_name + 1;
+        $newName = date('Ymdhis')."4";
         $newName = strval($newName) . "." . end($temp);
-        $last_name += 1;
         move_uploaded_file($_FILES['image4']['tmp_name'], $target_dir . $newName);
         $image4 = $newName;
     }
 
      if (!empty($_FILES['image5']['name'])) {
         $temp = explode(".", $_FILES["image5"]["name"]);
-        $newName = $last_name + 1;
+        $newName = date('Ymdhis')."5";
         $newName = strval($newName) . "." . end($temp);
-        $last_name += 1;
         move_uploaded_file($_FILES['image5']['tmp_name'], $target_dir . $newName);
         $image5 = $newName;
     }
 
      if (!empty($_FILES['image6']['name'])) {
         $temp = explode(".", $_FILES["image6"]["name"]);
-        $newName = $last_name + 1;
+        $newName = date('Ymdhis')."6";
         $newName = strval($newName) . "." . end($temp);
-        $last_name += 1;
         move_uploaded_file($_FILES['image6']['tmp_name'], $target_dir . $newName);
         $image6 = $newName;
     }
