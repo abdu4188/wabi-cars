ba3ec6111eca84d12a26894211294a37915fd8eb
Abdulaziz Yesuf <abdulazizyesuf7@gmail.com>
2020-03-07 12:20:02 +0300

    remove navbar shadow lon landing page

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,12 @@ $(window).scroll(function (){
     if (scroll >=100) {
         $('.navbar').addClass('bg-light');
         $('.navbar').removeClass('bg-trasparent');
+        $('.navbar').removeClass('shadow-none');
 
     }
     else {
         $('.navbar').addClass('bg-trasparent');
+        $('.navbar').addClass('shadow-none');
         $('.navbar').removeClass('bg-light');
     }
 });
